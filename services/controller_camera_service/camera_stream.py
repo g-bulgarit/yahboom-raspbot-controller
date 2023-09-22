b0842567d44beffc82bfaa178bba3278520bbaa4
g-Bulgarit <alonio6@gmail.com>
2023-09-22 22:06:43 +0300

    add: openCV stream to screen

---
+++ services/controller_camera_service/camera_stream.py
@@ -0,0 +1,14 @@
+import cv2
+
+if __name__ == "__main__":
+    vcap = cv2.VideoCapture("rtsp://192.168.1.197:8554/test")
+    fps = vcap.get(cv2.CAP_PROP_FPS)
+    print(fps)
+    if not vcap.isOpened():
+        print("Failed to open stream")
+        exit(0)
+
+    while 1:
+        ret, frame = vcap.read()
+        cv2.imshow("VIDEO", frame)
+        cv2.waitKey(1)
